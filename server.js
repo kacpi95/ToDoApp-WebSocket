fafc85c1c68661c3cbf18e66f12a408b79939f7f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-10 19:21:33 +0200

    add socket emit do new user

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const randomId = require('shortid');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'clinet')));
+const server = app.listen(8000, () => {
+  console.log('Server is running port: 8000');
+});
 const io = socket(server);
+
 const tasks = [
   { id: 1, name: 'Shopping' },
   {
@@ -12,8 +17,6 @@ const tasks = [
     name: 'Cleaning',
   },
 ];
-app.use(express.static(path.join(__dirname, 'clinet')));
-
-const server = app.listen(8000, () => {
-  console.log('Server is running port: 8000');
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
 });
