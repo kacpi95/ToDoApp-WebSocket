a3e81f6e059750023b408e8cc3b710a20a3ad766
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-10 19:10:49 +0200

    add basic structure to server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
+const randomId = require('shortid');
+const path = require('path');
 
 const app = express();
+const io = socket(server);
+const tasks = [
+  { id: 1, name: 'Shopping' },
+  {
+    id: 2,
+    name: 'Cleaning',
+  },
+];
+app.use(express.static(path.join(__dirname, 'clinet')));
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running port: 8000');
 });
