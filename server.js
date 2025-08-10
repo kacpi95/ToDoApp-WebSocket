131b4e0e13a5c7e469e1486a0f2a70b41ea4005b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-10 19:35:05 +0200

    add socket - addTask, removeTask

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const randomId = require('shortid');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'clinet')));
+app.use(express.static(path.join(__dirname, 'client')));
 const server = app.listen(8000, () => {
   console.log('Server is running port: 8000');
 });
@@ -19,4 +19,18 @@ const tasks = [
 ];
 io.on('connection', (socket) => {
   socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    if (!task.id) {
+      task.id = randomId.generate();
+    }
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (id) => {
+    const index = tasks.findIndex((el) => el.id === id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      socket.broadcast.emit('removeTask', id);
+    }
+  });
 });
